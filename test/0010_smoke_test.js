436066350725a3bc02483a3fb0c9ffbd3312c397
Dmitry Lapshov <kit@ppmlite.com>
2015-08-05 14:21:10 +0000

    Adding IndResource page

--- test/0010_smoke_test.js
+++ test/0010_smoke_test.js
@@ -340,6 +340,96 @@ test.describe('Smoke Test', function() {
         });
     });
     
+    test.it('/IndResource.aspx', function() {
+        driver.get(base + '/Resources.aspx');
+        driver.getTitle().then(function(title) {
+            assert.equal(title, 'Roadmap > Resources');
+        });
+        driver.wait(function() {
+            return driver.isElementPresent(By.xpath('//span[. = "Showing 1-4 from 4 Items"]'));
+        }, timeout);
+        driver.isElementPresent(By.xpath('//span[. = "Default View"]')).then(function(found) {
+            assert(found);
+        });
+        driver.findElement(By.xpath('//a[. = "Account Owner (You)"]')).click();
+        
+        driver.wait(until.titleIs('Roadmap > Account Owner'));
+        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        
+        driver.findElement(By.xpath('//a[@id = "tabWorkItems"]')).click();
+        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        driver.findElement(By.xpath('//div[@id = "projectWorkItemsTab"]')).then(function(element) {
+            driver.wait(until.elementIsVisible(element), timeout);
+        });
+        
+        driver.findElement(By.xpath('//a[@id = "tabCalendar"]')).click();
+        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        driver.findElement(By.xpath('//div[@id = "resourceCalendarTab"]')).then(function(element) {
+            driver.wait(until.elementIsVisible(element), timeout);
+        });
+        
+        driver.findElement(By.xpath('//a[@id = "tabAvailability"]')).click();
+        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        driver.findElement(By.xpath('//div[@id = "resourceAvailabilityTab"]')).then(function(element) {
+            driver.wait(until.elementIsVisible(element), timeout);
+        });
+        
+        driver.findElement(By.xpath('//a[@id = "tabIssues"]')).click();
+        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        driver.findElement(By.xpath('//div[@id = "projectIssuesTab"]')).then(function(element) {
+            driver.wait(until.elementIsVisible(element), timeout);
+        });
+        
+        driver.findElement(By.xpath('//a[@id = "tabNotes"]')).click();
+        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        driver.findElement(By.xpath('//div[@id = "projectNotesTab"]')).then(function(element) {
+            driver.wait(until.elementIsVisible(element), timeout);
+        });
+        
+        driver.findElement(By.xpath('//a[@id = "tabAttachments"]')).click();
+        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        driver.findElement(By.xpath('//div[@id = "projectAttachmentsTab"]')).then(function(element) {
+            driver.wait(until.elementIsVisible(element), timeout);
+        });
+        
+        driver.findElement(By.xpath('//a[@id = "tabTimes"]')).click();
+        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        driver.findElement(By.xpath('//div[@id = "resourceTimesTab"]')).then(function(element) {
+            driver.wait(until.elementIsVisible(element), timeout);
+        });
+        
+        driver.findElement(By.xpath('//a[@id = "rSubscriptions"]')).click();
+        driver.wait(function() {
+            return driver.isElementPresent(By.xpath('//div[@id = "rSubscriptionsPanel" and @expanded = "1"]'));
+        }, timeout);
+        
+        driver.findElement(By.xpath('//a[@id = "rSettings"]')).click();
+        driver.wait(function() {
+            return driver.isElementPresent(By.xpath('//div[@id = "rSettingsPanel" and @expanded = "1"]'));
+        }, timeout);
+
+        driver.findElement(By.xpath('//a[@id = "rSettings"]')).click();
+        driver.wait(function() {
+            return driver.isElementPresent(By.xpath('//div[@id = "rSettingsPanel" and @expanded = "0"]'));
+        }, timeout);
+    });
+    
     test.it('/Time.aspx', function() {
         driver.get(base + '/Time.aspx');
         driver.getTitle().then(function(title) {
