f71a226c96b98cbce787657c0e21a8d9dadd2a58
Dmitry Lapshov <kit@ppmlite.com>
2015-08-12 13:15:41 +0000

    Update access groups

--- test/0020_user_access_groups.js
+++ test/0020_user_access_groups.js
@@ -26,79 +26,96 @@ test.describe('User Access Groups', function() {
     });
 
     test.it('/Login.aspx', function() {
-        driver.get(base + '/Account.aspx');
+        driver.get(base + '/UserGroups.aspx');
         driver.findElement(By.xpath('//input[@id = "Login1_UserName"]')).sendKeys(user);
         driver.findElement(By.xpath('//input[@id = "Login1_Password"]')).sendKeys('1234567');
         driver.findElement(By.xpath('//input[@id = "Login1_LoginButton"]')).click();
-        driver.wait(until.titleIs('Roadmap > Account > My Account'));
+        driver.wait(until.titleIs('Roadmap > Resources > User Access Groups'), timeout);
     });
     
     test.it('Should be possible to create user access groups', function() {
-        driver.get(base + '/UserGroups.aspx');
-        driver.getTitle().then(function(title) {
-            assert.equal(title, 'Roadmap > Resources > User Access Groups');
-        });
-        driver.findElement(By.xpath('//a[contains(., "Add User Access Group")]')).click();
-        
+        driver.get(base + '/UserGroupAdd.aspx');
         driver.wait(until.titleIs('Roadmap > Resources > Add Group'));
+        
         driver.findElement(By.xpath('//input[@id = "txtUserAccessGroupName"]')).sendKeys('Simple ' + uniqueID);
         driver.findElement(By.xpath('//div[@class = "tblProjects-project-name" and . = "Sample Project A"]/../..//input[@type = "checkbox"]')).click();
         driver.findElement(By.xpath('//a[. = "Save and add another"]')).click();
         
         driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
-            driver.wait(until.elementIsNotVisible(element));
+            driver.wait(until.elementIsNotVisible(element), timeout);
         });
-        driver.findElement(By.xpath('//input[@id = "txtUserAccessGroupName"]')).sendKeys('Can Create Projects ' + uniqueID);
+        
+        driver.findElement(By.xpath('//input[@id = "txtUserAccessGroupName"]')).sendKeys('Create ' + uniqueID);
         driver.findElement(By.xpath('//div[@class = "tblProjects-project-name" and . = "Sample Project A"]/../..//input[@type = "checkbox"]')).click();
         driver.findElement(By.xpath('//input[@id = "chkAllowCreateProjects"]')).click();
         driver.findElement(By.xpath('//a[. = "Save and add another"]')).click();
         
         driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
-            driver.wait(until.elementIsNotVisible(element));
+            driver.wait(until.elementIsNotVisible(element), timeout);
         });
-        driver.findElement(By.xpath('//input[@id = "txtUserAccessGroupName"]')).sendKeys('Can See All Projects ' + uniqueID);
+        
+        driver.findElement(By.xpath('//input[@id = "txtUserAccessGroupName"]')).sendKeys('All ' + uniqueID);
         driver.findElement(By.xpath('//div[@class = "tblProjects-project-name" and . = "Sample Project A"]/../..//input[@type = "checkbox"]')).click();
         driver.findElement(By.xpath('//input[@id = "chkHybridGroup"]')).click();
         driver.findElement(By.xpath('//a[. = "Save and add another"]')).click();
         
         driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
-            driver.wait(until.elementIsNotVisible(element));
+            driver.wait(until.elementIsNotVisible(element), timeout);
         });
-        driver.findElement(By.xpath('//input[@id = "txtUserAccessGroupName"]')).sendKeys('To Be Edited ' + uniqueID);
-        driver.findElement(By.xpath('//div[@class = "tblProjects-project-name" and . = "Sample Project A"]/../..//input[@type = "checkbox"]')).click();
-        driver.findElement(By.xpath('//a[. = "Save and add another"]')).click();
         
-        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
-            driver.wait(until.elementIsNotVisible(element));
-        });
-        driver.findElement(By.xpath('//input[@id = "txtUserAccessGroupName"]')).sendKeys('To Be Deleted ' + uniqueID);
+        driver.findElement(By.xpath('//input[@id = "txtUserAccessGroupName"]')).sendKeys('Test ' + uniqueID);
         driver.findElement(By.xpath('//div[@class = "tblProjects-project-name" and . = "Sample Project A"]/../..//input[@type = "checkbox"]')).click();
-        driver.findElement(By.xpath('//div[@class = "user-access-group-button-panel"]/input[@value = "Save"]')).click();
+        driver.findElement(By.xpath('//input[@value = "Save"]')).click();
 
-        driver.wait(until.titleIs('Roadmap > Resources > User Access Groups'));        
-        driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[contains(., "Simple")]')).then(function(found) {
+        driver.wait(until.titleIs('Roadmap > Resources > User Access Groups'));           
+        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+
+        driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[. = "Simple '+ uniqueID +'"]')).then(function(found) {
             assert(found);
         });
-        driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[contains(., "Can Create Projects")]')).then(function(found) {
+        driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[. = "Create '+ uniqueID +'"]')).then(function(found) {
             assert(found);
         });
-        driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[contains(., "Can See All Projects")]')).then(function(found) {
+        driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[. = "All '+ uniqueID +'"]')).then(function(found) {
             assert(found);
         });
-        driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[contains(., "To Be Edited")]')).then(function(found) {
-            assert(found);
-        });
-        driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[contains(., "To Be Deleted")]')).then(function(found) {
+        driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[. = "Test '+ uniqueID +'"]')).then(function(found) {
             assert(found);
         });
     });
     
     test.it('Should be possible to edit user access groups', function() {
-        assert(false);
+        driver.findElement(By.xpath('//table[@id = "uaGroups"]//a[. = "Test '+ uniqueID +'"]')).click();
+        driver.wait(until.titleIs('Roadmap > Resources > Edit Group'), timeout);
+        
+        driver.findElement(By.xpath('//div[@id = "uiblocker"]')).then(function(element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        
+        driver.findElement(By.xpath('//input[@id = "txtUserAccessGroupName"]')).then(function(element) {
+            element.clear();
+            element.sendKeys('Changed ' + uniqueID);
+        });
+        
+        driver.findElement(By.xpath('//input[@value = "Save"]')).click();
+        
+        driver.wait(until.titleIs('Roadmap > Resources > User Access Groups'), timeout);
+        driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[. = "Changed '+ uniqueID +'"]')).then(function(found) {
+            assert(found);
+        });
     });
     
     test.it('Should be possible to delete user access groups', function() {
-        assert(false);
+        driver.findElement(By.xpath('//a[. = "Changed '+ uniqueID +'"]/../..//div[. = "Delete"]')).click();
+        driver.findElement(By.xpath('//span[. = "Delete"]')).click();
+        
+        driver.wait(function() {
+            return driver.isElementPresent(By.xpath('//table[@id = "uaGroups"]//a[. = "Changed '+ uniqueID +'"]')).then(function(found) {
+                return !found;
+            });
+        }, timeout);
     });
     
 });
