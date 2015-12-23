32a1708bea51e5f2087980530ece42c74cabfd4e
DmitryLapshov <kit@ppmroadmap.com>
2015-12-23 16:30:19 +0200

    Update script

--- test/0100_create_dash_view_B.js
+++ test/0100_create_dash_view_B.js
@@ -59,146 +59,43 @@ test.describe('Create Dashboard View B', function () {
         driver.sleep(pause);
     });
 
-    test.it('Should be possible to add Project Status Widget', function () {
+    test.it('Should be possible to add Free Text Field 1x1 Widget', function () {
         driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "1" and @data-col = "2"]')).click();
-        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[. = "Project Status"]')), timeout).then(function (element) {
+        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[. = "Free Text Field 1x1"]')), timeout).then(function (element) {
             element.click();
         });
-        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Project Status"]')), timeout);
-        driver.sleep(pause);
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@id = "showLabel"]')).click();
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
-        driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
-            driver.wait(until.elementIsNotVisible(element), timeout);
-        });
-        driver.wait(until.elementLocated(By.xpath('//div[@class = "projectStatus"]')), timeout);
-    });
-
-    test.it('Should be possible to add Project Health Widget', function () {
-        driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "1" and @data-col = "3"]')).click();
-        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[. = "Project Health"]')), timeout).then(function (element) {
-            element.click();
-        });
-        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Project Health"]')), timeout);
-        driver.sleep(pause);
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@id = "showLabel"]')).click();
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
-        driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
-            driver.wait(until.elementIsNotVisible(element), timeout);
-        });
-        driver.wait(until.elementLocated(By.xpath('//div[@class = "portfolioHealth"]')), timeout);
-    });
-
-    test.it('Should be possible to add Project Attributes Widget', function () {
-        driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "1" and @data-col = "4"]')).click();
-        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[. = "Project Attributes"]')), timeout).then(function (element) {
-            element.click();
-        });
-        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Project Attributes"]')), timeout);
-        driver.sleep(pause);
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@id = "nocTitle"]')).sendKeys('Project Attributes');
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//span[. = "select"]')).click();
-        driver.sleep(pause);
-        driver.findElement(By.xpath('//ul[@id = "nocAttr_listbox" and @aria-hidden = "false"]/li[. = "Client"]')).click();
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@id = "showLabel"]')).click();
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
-        driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
-            driver.wait(until.elementIsNotVisible(element), timeout);
-        });
-        driver.wait(until.elementLocated(By.xpath('//div[@class = "numberOrCurrency"]')), timeout);
-    });
-
-    test.it('Should be possible to add Schedule Accuracy Widget', function () {
-        driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "1" and @data-col = "5"]')).click();
-        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[. = "Schedule Accuracy"]')), timeout).then(function (element) {
-            element.click();
-        });
-        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Schedule Accuracy"]')), timeout);
-        driver.sleep(pause);
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
-        driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
-            driver.wait(until.elementIsNotVisible(element), timeout);
-        });
-        driver.wait(until.elementLocated(By.xpath('//div[@class = "portfolioSchedule"]')), timeout);
-    });
-
-    test.it('Should be possible to add Role Utilization Widget', function () {
-        driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "2" and @data-col = "1"]')).click();
-        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[. = "Role Utilization"]')), timeout).then(function (element) {
-            element.click();
-        });
-        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Role Utilization"]')), timeout);
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//span[. = "Select Role"]/..//span[. = "select"]')).click();
-        driver.sleep(pause);
-        driver.findElement(By.xpath('//ul[@id = "resRole_listbox" and @aria-hidden = "false"]//span[. = "Analyst"]')).click();
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@id = "showLabel"]')).click();
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
-        driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
-            driver.wait(until.elementIsNotVisible(element), timeout);
-        });
-        driver.wait(until.elementLocated(By.xpath('//div[@class = "roleAvailability"]//div[@id = "rolechart"]')), timeout);
-    });
-
-    test.it('Should be possible to add Role Availability Widget', function () {
-        driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "2" and @data-col = "2"]')).click();
-        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[. = "Role Availability"]')), timeout).then(function (element) {
-            element.click();
-        });
-        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Role Availability"]')), timeout);
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//span[. = "Select Role"]/..//span[. = "select"]')).click();
-        driver.sleep(pause);
-        driver.findElement(By.xpath('//ul[@id = "resResource_listbox" and @aria-hidden = "false"]//span[. = "Analyst"]')).click();
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
-        driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
-            driver.wait(until.elementIsNotVisible(element), timeout);
-        });
-        driver.wait(until.elementLocated(By.xpath('//div[@class = "resourceAvailability"]//div[@id = "roleAChart"]')), timeout);
-    });
-
-    test.it('Should be possible to add Resource Utilization Widget', function () {
-        driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "3" and @data-col = "1"]')).click();
-        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[contains(., "Resource Utilization")]')), timeout).then(function (element) {
-            element.click();
-        });
-        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Resource Utilization"]')), timeout);
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//span[. = "Select Resource"]/..//span[. = "select"]')).click();
-        driver.sleep(pause);
-        driver.findElement(By.xpath('//ul[@id = "resResource_listbox" and @aria-hidden = "false"]//span[. = "Charlie Sample"]')).click();
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@id = "showLabel"]')).click();
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
-        driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
-            driver.wait(until.elementIsNotVisible(element), timeout);
-        });
-        driver.wait(until.elementLocated(By.xpath('//div[@class = "roleAvailability"]//div[@id = "resourceUChart"]')), timeout);
-    });
-
-    test.it('Should be possible to add Resource Availability Widget', function () {
-        driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "3" and @data-col = "2"]')).click();
-        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[contains(., "Resource Availability")]')), timeout).then(function (element) {
-            element.click();
-        });
-        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Resource Availability"]')), timeout);
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//span[. = "Select Resource"]/..//span[. = "select"]')).click();
-        driver.sleep(pause);
-        driver.findElement(By.xpath('//ul[@id = "resResource_listbox" and @aria-hidden = "false"]//span[. = "Charlie Sample"]')).click();
-        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
-        driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
-            driver.wait(until.elementIsNotVisible(element), timeout);
-        });
-        driver.wait(until.elementLocated(By.xpath('//div[@class = "resourceAvailability"]//div[@id = "chart"]')), timeout);
-    });
-
-    test.it('Should be possible to add Free Text Field 2x2 Widget', function () {
-        driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "2" and @data-col = "4"]')).click();
-        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[. = "Free Text Field 2x2"]')), timeout).then(function (element) {
-            element.click();
-        });
-        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Free Text Field 2x2"]')), timeout);
+        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Free Text Field 1x1"]')), timeout);
         driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
         driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
             driver.wait(until.elementIsNotVisible(element), timeout);
         });
         driver.wait(until.elementLocated(By.xpath('//div[contains(@class, "textEditor")]')), timeout);
+    });
+
+    test.it('Should be possible to add Free Text Field 2x1 Widget', function () {
+        driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "1" and @data-col = "3"]')).click();
+        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[. = "Free Text Field 2x1"]')), timeout).then(function (element) {
+            element.click();
+        });
+        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Free Text Field 2x1"]')), timeout);
+        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
+        driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        driver.wait(until.elementLocated(By.xpath('//div[contains(@class, "textEditor")]')), timeout);
+    });
+
+    test.it('Should be possible to add Completed Items Widget', function () {
+        driver.findElement(By.xpath('//div[@id = "dashboard"]//li[@data-row = "1" and @data-col = "4"]')).click();
+        driver.wait(until.elementLocated(By.xpath('//div[@id = "addWidgetForm"]//div[. = "Completed Items"]')), timeout).then(function (element) {
+            element.click();
+        });
+        driver.wait(until.elementLocated(By.xpath('//section[@class = "previewHolder"]//span[. = "Add Completed Items"]')), timeout);
+        driver.findElement(By.xpath('//section[@class = "previewHolder"]//input[@class = "btn-addwidget"]')).click();
+        driver.findElement(By.xpath('//div[@id = "addWidgetForm"]')).then(function (element) {
+            driver.wait(until.elementIsNotVisible(element), timeout);
+        });
+        driver.wait(until.elementLocated(By.xpath('//span[. = "Completed This Week"]')), timeout);
     });
 
 });
